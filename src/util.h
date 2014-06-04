59d1845374428319f21c5de1e71cd9d65a24f251
CHEN YUJIA <nearc116@hotmail.com>
2014-06-05 01:33:31 +0800

    local change

--- src/util.h
+++ src/util.h
@@ -11,7 +11,7 @@
 /* Procedure printToken prints a token 
  * and its lexeme to the listing file
  */
-void printToken( TokenType, const char* );
+void printToken( TokenType token, const char* );
 
 /* Function newStmtNode creates a new statement
  * node for syntax tree construction
@@ -23,11 +23,14 @@ TreeNode * newStmtNode(StmtKind);
  */
 TreeNode * newExpNode(ExpKind);
 
+/* Function newModuleNode creates a new Module
+ * node for syntax tree construction
+ */
+TreeNode * newModuleNode(ModuleKind);
+
 /* Function copyString allocates and makes a new
  * copy of an existing string
  */
-
-TreeNode * newModuleNode(ModuleKind);
 char * copyString( char * );
 
 /* procedure printTree prints a syntax tree to the 
